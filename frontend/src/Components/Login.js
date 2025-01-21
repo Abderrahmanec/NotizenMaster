0a5de12c69d76a14ff819938791285d3504eceda
Abderrahmane <mm66ahmed20@gmail.com>
2025-01-21 10:49:20 +0100

    API-URL von 192.168.178.185 zu localhost geändert

--- frontend/src/Components/Login.js
+++ frontend/src/Components/Login.js
@@ -89,7 +89,7 @@ function Login() {
     }
 
     // Login-Anfrage senden
-    fetch("http://localhost:8080/api/auth/login", {
+    fetch("http://localhost:8080/api/auth/login",{
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify(formData),
