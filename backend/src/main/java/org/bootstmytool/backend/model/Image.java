c63341eae86e38800d10580caf7ec1a04f8b6bfe
Abderrahmane <mm66ahmed20@gmail.com>
2025-01-25 18:32:36 +0100

    Anpassung Erstellungsdatum, Image url und getUrl()

--- backend/src/main/java/org/bootstmytool/backend/model/Image.java
+++ backend/src/main/java/org/bootstmytool/backend/model/Image.java
@@ -1,5 +1,6 @@
 package org.bootstmytool.backend.model;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
 import jakarta.persistence.*;
 
 /**
@@ -7,19 +8,24 @@ import jakarta.persistence.*;
  * Das Bild wird in der Datenbank als Blob gespeichert und kann mit einer Notiz in einer "Viele-zu-Eins"-Beziehung verbunden werden.
  */
 @Entity
+@Table(name = "image")
 public class Image {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id; // Die eindeutige ID des Bildes
 
+    private String url; // Die URL des Bildes
     @Lob
     private byte[] data; // Die Binärdaten des Bildes (in der Regel als Blob gespeichert)
 
     @ManyToOne
     @JoinColumn(name = "note_id")
-    private org.bootstmytool.backend.model.Note note; // Die Notiz, zu der dieses Bild gehört
+    @JsonBackReference
+    private Note note; // Die Notiz, zu der dieses Bild gehört
 
+    @Column(name = "created_date")
+    private java.time.LocalDateTime createdDate; // Neues Feld
     // Konstruktoren, Getter und Setter
 
     /**
@@ -27,6 +33,7 @@ public class Image {
      * Wird von JPA benötigt.
      */
     public Image() {
+        this.createdDate = java.time.LocalDateTime.now();
     }
 
     /**
@@ -84,13 +91,16 @@ public class Image {
     }
 
     //set and get methods fuer url
-    public void setUrl(String s) {
-        throw new UnsupportedOperationException("Nicht unterstützt."); //To change body of generated methods, choose Tools | Templates.
+    public void setUrl(String url) {
+        this.url = url;
     }
 
     public String getUrl() {
-        throw new UnsupportedOperationException("Nicht unterstützt."); //To change body of generated methods, choose Tools | Templates.
+        return url;
     }
+
+
+
 }
 
 
