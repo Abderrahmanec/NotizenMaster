0f8dd491485dbd6cde10cfe4c95fb7dfe3ad3ca9
Abderrahmane <mm66ahmed20@gmail.com>
2025-01-25 18:45:32 +0100

    Füge Berechtigung für den Zugriff auf /image/** Endpunkt in der Sicherheitskonfiguration hinzu

--- backend/src/main/java/org/bootstmytool/backend/security/SecurityConfig.java
+++ backend/src/main/java/org/bootstmytool/backend/security/SecurityConfig.java
@@ -17,8 +17,6 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-
 import java.util.Arrays;
 
 /**
