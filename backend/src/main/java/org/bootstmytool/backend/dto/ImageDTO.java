8b28c7db2d0d5406feb9b5d8ae613c2726924726
Abderrahmane <mm66ahmed20@gmail.com>
2025-01-25 18:30:40 +0100

    Neue ImageDTO-Klasse hinzugefügt, die die Bild-URL und Metadaten enthält

--- backend/src/main/java/org/bootstmytool/backend/dto/ImageDTO.java
+++ backend/src/main/java/org/bootstmytool/backend/dto/ImageDTO.java
@@ -1,4 +1,38 @@
 package org.bootstmytool.backend.dto;
 
 public class ImageDTO {
+    private String url;
+
+    /**
+     * Erstellt ein neues ImageDTO.
+     */
+    public ImageDTO() {
+    }
+
+    /**
+     * Erstellt ein neues ImageDTO.
+     *
+     * @param url Die URL des Bildes.
+     */
+    public ImageDTO(String url) {
+        this.url = url;
+    }
+
+    /**
+     * Gibt die URL des Bildes zurueck.
+     *
+     * @return Die URL des Bildes.
+     */
+    public String getUrl() {
+        return url;
+    }
+
+    /**
+     * Setzt die URL des Bildes.
+     *
+     * @param url Die URL des Bildes.
+     */
+    public void setUrl(String url) {
+        this.url = url;
+    }
 }
