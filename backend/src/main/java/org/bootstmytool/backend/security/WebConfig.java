007a560d044915601af5ae0a483526810523b3ab
Abderrahmane <mm66ahmed20@gmail.com>
2025-01-25 18:47:54 +0100

    Korrigiere den Pfad zum Bild in der WebConfig-Klasse

--- backend/src/main/java/org/bootstmytool/backend/security/WebConfig.java
+++ backend/src/main/java/org/bootstmytool/backend/security/WebConfig.java
@@ -1,4 +1,21 @@
 package org.bootstmytool.backend.security;
 
-public class WebConfig {
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        // Serve images from the "static/images" directory in the frontend
+        registry.addResourceHandler("/static/**")
+                .addResourceLocations("classpath:/frontend/static/images/");
+
+       // Bild aus dem Ordner images
+        registry.addResourceHandler("/image/**")
+                .addResourceLocations("classpath:/frontend/images/"); // Pfad zu den Bildern
+    }
 }
