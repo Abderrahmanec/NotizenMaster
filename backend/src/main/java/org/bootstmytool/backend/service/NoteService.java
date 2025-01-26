01ef5b0672d9a95f455cf81d985263f2b273ccd9
Abderrahmane <mm66ahmed20@gmail.com>
2025-01-26 18:18:51 +0100

    Service für Bearbeiten und Löschen einer Notiz

--- backend/src/main/java/org/bootstmytool/backend/service/NoteService.java
+++ backend/src/main/java/org/bootstmytool/backend/service/NoteService.java
@@ -62,4 +62,25 @@ public class NoteService {
     public List<Note> getNotesByUserId(int id) {
         return noteRepository.findByUserId(id);
     }
+
+
+
+    public String deleteNoteById(int id) {
+        Note existingNote = noteRepository.findById(id).orElse(null);
+        if (existingNote != null) {
+            noteRepository.delete(existingNote);
+            return "Note deleted successfully!";
+        }
+        return "Note not found!";
+    }
+
+    public Note editNoteById(int id, Note note) {
+        Note existingNote = noteRepository.findById(id).orElse(null);
+        if (existingNote != null) {
+            existingNote.setTitle(note.getTitle());
+            existingNote.setContent(note.getContent());
+            return noteRepository.save(existingNote);
+        }
+        return null;
+    }
 }
