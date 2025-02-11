7397b420b04b85b056da91ef0aae4113de3c7aee
Abderrahmane <mm66ahmed20@gmail.com>
2025-02-11 18:07:10 +0100

    Implementiere Methode zum Suchen von Notizen anhand eines Suchbegriffs
    - Nutzt die Repository-Methode indByTitleContainingOrContentContaining, um Notizen basierend auf dem Titel oder Inhalt zu suchen, die den angegebenen Suchbegriff enthalten.

--- backend/src/main/java/org/bootstmytool/backend/service/NoteService.java
+++ backend/src/main/java/org/bootstmytool/backend/service/NoteService.java
@@ -83,4 +83,8 @@ public class NoteService {
         }
         return null;
     }
+
+    public List<Note> searchNotes(String searchTerm) {
+        return noteRepository.findByTitleContainingOrContentContaining(searchTerm, searchTerm);
+    }
 }
