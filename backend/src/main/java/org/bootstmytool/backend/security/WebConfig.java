4f5052c31cc7f372d6d3d7080f14c926bbff5dbe
Abderrahmane <mm66ahmed20@gmail.com>
2025-02-20 01:24:44 +0100

    Aktualisiere Ressourcen-Handler für Bilder

--- backend/src/main/java/org/bootstmytool/backend/security/WebConfig.java
+++ backend/src/main/java/org/bootstmytool/backend/security/WebConfig.java
@@ -7,15 +7,12 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
-
+    //die Methode addResourceHandlers() wird ueberschrieben, um die Konfiguration fuer die Ressourcen-Handler zu aendern
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         // Serve images from the "static/images" directory in the frontend
-        registry.addResourceHandler("/static/**")
-                .addResourceLocations("classpath:/frontend/static/images/");
+        registry.addResourceHandler("/images/**")
+                .addResourceLocations("classpath:/backend/static/images/");
 
-       // Bild aus dem Ordner images
-        registry.addResourceHandler("/image/**")
-                .addResourceLocations("classpath:/frontend/images/"); // Pfad zu den Bildern
     }
 }
