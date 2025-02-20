9c7e2bdab480167eda4ba97f4237082bb705bb14
Abderrahmane <mm66ahmed20@gmail.com>
2025-02-20 01:24:48 +0100

    Füge Methoden zur Bildverwaltung hinzu

--- backend/src/main/java/org/bootstmytool/backend/service/ImageService.java
+++ backend/src/main/java/org/bootstmytool/backend/service/ImageService.java
@@ -1,23 +1,31 @@
 package org.bootstmytool.backend.service;
 
+import jakarta.transaction.Transactional;
 import org.bootstmytool.backend.model.Image;
 import org.bootstmytool.backend.model.Note;
 import org.bootstmytool.backend.repository.ImageRepository;
 import org.bootstmytool.backend.repository.NoteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
+import java.net.MalformedURLException;
 import java.util.List;
+import java.util.Optional;
+
 
 @Service
 public class ImageService {
 
-  //fuegt die NoteRepository und ImageRepository Instanzen hinzu
+    //fuegt die NoteRepository und ImageRepository Instanzen hinzu
     private final NoteRepository noteRepository;
     private final ImageRepository imageRepository;
 
 
-  //fuegt die ImageRepository und NoteRepository Instanzen hinzu
+    //fuegt die ImageRepository und NoteRepository Instanzen hinzu
     @Autowired
     public ImageService(NoteRepository noteRepository, ImageRepository imageRepository) {
         this.noteRepository = noteRepository;
@@ -29,4 +37,86 @@ public class ImageService {
     public List<Image> getImagesByNoteId(int noteId) {
         return imageRepository.findByNoteId(noteId);
     }
+
+
+//loeche ein Image Objekt aus der Datenbank
+@Transactional
+public boolean deleteImageById(int imgId) {
+    Optional<Image> imageOpt = imageRepository.findById(imgId);
+
+    if (imageOpt.isPresent()) {
+        Image image = imageOpt.get();
+        String imagePath = "backend/src/main/resources/static/images/" + image.getUrl(); // Update path
+
+        // Delete file from server
+        File file = new File(imagePath);
+        if (file.exists()) {
+            if (file.delete()) {
+            } else {
+                throw new RuntimeException("Failed to delete image file");
+            }
+        } else {
+
+        }
+
+
+        // Remove reference from associated Notes
+        List<Note> notes = noteRepository.findByImagesContaining((image));
+        for (Note note : notes) {
+            note.getImages().remove(image);
+            noteRepository.save(note);  // Save the updated Note entity
+        }
+
+
+        // Delete entry from database
+        imageRepository.deleteById(imgId);;
+
+        return true;
+    } else {
+        return false;
+    }
+
+}
+
+
+
+//speichert ein Image Objekt in der Datenbank
+    public String saveImage(MultipartFile file) {
+        // Save the image to the file system
+        String imagePath = "images/" + file.getOriginalFilename();
+        File imageFile = new File(imagePath);
+        try {
+            file.transferTo(imageFile);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return imagePath;
+    }
+
+
+    //gibt Resource Objekt zurueck
+    public Resource getImage(int imageId) {
+        Image image = imageRepository.findById(imageId).orElse(null);
+        if (image == null) {
+            return null;
+        }
+        try {
+            return new UrlResource("file:" + image.getUrl());
+        } catch (MalformedURLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public String getImageUrl(int imageId) {
+        Image image = imageRepository.findById(imageId).orElse(null);
+        if (image == null) {
+            return null;
+        }
+        return image.getUrl();
+    }
+
+
+
+
 }
