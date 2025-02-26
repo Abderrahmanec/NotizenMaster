0d1aaf85001958d668f262a0650ba4e18683704a
Abderrahmane <mm66ahmed20@gmail.com>
2025-02-26 20:59:39 +0100

    WebConfig aktualisiert, um Ressourcen-Handler für Frontend-Bilder zu definieren

--- backend/src/main/java/org/bootstmytool/backend/security/WebConfig.java
+++ backend/src/main/java/org/bootstmytool/backend/security/WebConfig.java
@@ -4,13 +4,22 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+/**
+ * @Author Mohamed Cheikh
+ * @Version 1.0
+ * @Date: 2025-03-27
+ * Konfigurationsklasse für die Webanwendung.
+ * <p>
+ * Diese Klasse konfiguriert die Ressourcen-Handler für die Anwendung.
+ * Sie definiert, wie Ressourcen wie Bilder oder CSS-Dateien geladen werden.
+ */
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
     //die Methode addResourceHandlers() wird ueberschrieben, um die Konfiguration fuer die Ressourcen-Handler zu aendern
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        // Serve images from the "static/images" directory in the frontend
+        // Definiert, wie Ressourcen aus dem Verzeichnis "frontend/static" geladen werden
         registry.addResourceHandler("/images/**")
                 .addResourceLocations("classpath:/backend/static/images/");
 
