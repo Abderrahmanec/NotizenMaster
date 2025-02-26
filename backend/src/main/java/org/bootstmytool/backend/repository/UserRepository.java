a1f2c5317d58fd7ea9dd77ee75226039ea181bb7
Abderrahmane <mm66ahmed20@gmail.com>
2025-02-26 20:42:04 +0100

    Refaktorierung des UserRepository: Ersetzte findByUsername durch findByEmail und entfernte veraltete Methoden.

--- backend/src/main/java/org/bootstmytool/backend/repository/UserRepository.java
+++ backend/src/main/java/org/bootstmytool/backend/repository/UserRepository.java
@@ -9,6 +9,9 @@ import org.springframework.stereotype.Repository;
 import java.util.Optional;
 
 /**
+ * @Author Mohamed Cheikh
+ * @Version 1.0
+ * @Date: 2025-03-27
  * Das Repository für die User-Entität. Diese Schnittstelle erweitert JpaRepository,
  * um grundlegende CRUD-Operationen für die Benutzer zu ermöglichen und benutzerdefinierte
  * Abfragen durchzuführen.
@@ -19,26 +22,10 @@ public interface UserRepository extends JpaRepository<User, Integer> {
     /**
      * Findet einen Benutzer anhand des Benutzernamens.
      *
-     * @param username Der Benutzername des gesuchten Benutzers
+     * @param email Der Benutzername des gesuchten Benutzers
      * @return Ein Optional mit dem Benutzer, falls gefunden
      */
-    Optional<User> findByUsername(String username);
+    Optional<User> findByEmail(String email);
 
-    /**
-     * Findet einen Benutzer anhand des Benutzernamens und Passworts.
-     *
-     * @param username Der Benutzername des gesuchten Benutzers
-     * @param password Das Passwort des gesuchten Benutzers
-     * @return Ein Optional mit dem Benutzer, falls Benutzername und Passwort übereinstimmen
-     */
-    Optional<User> findByUsernameAndPassword(String username, String password);
 
-    /**
-     * Überprüft, ob der Benutzername bereits in der Datenbank existiert.
-     *
-     * @param username Der Benutzername, der überprüft werden soll
-     * @return Ein Optional mit dem Benutzer, falls der Benutzername existiert
-     */
-    @Query("SELECT u FROM User u WHERE u.username = :username")
-    Optional<User> checkIfUsernameExists(@Param("username") String username);
 }
