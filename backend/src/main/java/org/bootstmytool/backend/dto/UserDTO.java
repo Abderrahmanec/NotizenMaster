0d66edea4950d455ceb04c47463306ff7a5c600b
Abderrahmane <mm66ahmed20@gmail.com>
2025-02-26 20:02:04 +0100

    feat: Lombok-Annotationen für UserDTO hinzugefügt und Getter/Setter optimiert

--- backend/src/main/java/org/bootstmytool/backend/dto/UserDTO.java
+++ backend/src/main/java/org/bootstmytool/backend/dto/UserDTO.java
@@ -1,50 +1,40 @@
 package org.bootstmytool.backend.dto;
 
 import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
 /**
+ * @Author Mohamed Cheikh
+ * @version 1.0
+ * @Date: 2025-03-27
  * Die UserDTO (Data Transfer Object) Klasse wird verwendet, um Benutzerdaten wie
  * den Benutzernamen und das Passwort zwischen den verschiedenen Schichten der
  * Anwendung zu übertragen.
  */
+@Setter
+@Getter
 @Data
 public class UserDTO {
-    private String username; // Der Benutzername des Benutzers
+    /**
+     * -- GETTER --
+     *  Gibt den Benutzernamen des Benutzers zurück.
+     *
+     * -- SETTER --
+     *  Setzt den Benutzernamen des Benutzers.
+     *
+
+     */
+    private String email; // Der Benutzername des Benutzers
+    /**
+     * -- GETTER --
+     *  Gibt das Passwort des Benutzers zurück.
+     *
+     * -- SETTER --
+     *  Setzt das Passwort des Benutzers.
+     *
+
+     */
     private String password; // Das Passwort des Benutzers
 
-    /**
-     * Setzt den Benutzernamen des Benutzers.
-     *
-     * @param username Der Benutzername, der gesetzt werden soll
-     */
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    /**
-     * Setzt das Passwort des Benutzers.
-     *
-     * @param password Das Passwort, das gesetzt werden soll
-     */
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    /**
-     * Gibt den Benutzernamen des Benutzers zurück.
-     *
-     * @return Der Benutzername des Benutzers
-     */
-    public String getUsername() {
-        return username;
-    }
-
-    /**
-     * Gibt das Passwort des Benutzers zurück.
-     *
-     * @return Das Passwort des Benutzers
-     */
-    public String getPassword() {
-        return password;
-    }
 }
