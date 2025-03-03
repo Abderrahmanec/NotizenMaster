80037566fb06b30d85d9103c33d25a1c008c7426
Abderrahmane <mm66ahmed20@gmail.com>
2025-03-03 22:05:04 +0100

    Endpoint

--- backend/src/main/java/org/bootstmytool/backend/security/SecurityConfig.java
+++ backend/src/main/java/org/bootstmytool/backend/security/SecurityConfig.java
@@ -123,7 +123,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration corsConfiguration = new CorsConfiguration();
-        corsConfiguration.setAllowedOrigins(Arrays.asList("http://localhost:3000", "http://localhost:3001", "http://192.168.178.144:3000")); // Frontend-URLs erlauben
+        corsConfiguration.setAllowedOrigins(Arrays.asList("http://localhost:3000", "http://localhost:3001", "https://notizen-master.vercel.app/")); // Frontend-URLs erlauben
         corsConfiguration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS")); // Erlaubte HTTP-Methoden
         corsConfiguration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type", "X-Requested-With")); // Erlaubte Header
         corsConfiguration.setAllowCredentials(true); // Erlaubt Cookies und Authentifizierung
